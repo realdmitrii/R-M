3a645f371cc0367b78edf2c488aab9a6305dbc51
realdmitrii <realdmitriii@gmail.com>
2025-11-07 14:33:11 +0500

    style: linter

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
@@ -18,6 +19,13 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+  },
+
+  {
+    plugins: {},
+    rules: {
+      'no-console': 'warn',
     },
   },
 ]);
